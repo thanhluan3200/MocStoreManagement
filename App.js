6938c7c47f9bd3a2f8d30f45cdcb477843e169fe
Nguyễn Thành Luân <62294084+thanhluan3200@users.noreply.github.com>
2021-12-13 01:24:09 +0700

    Update App.js
    import component

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import TotalProducts from './Component/TotalProducts';
+import Product from './Component/Product'
+import Turnover from './Component/Turnover'
+import Posts from './Component/Posts'
+import Customers from './Component/Customers'
+import Orders from './Component/Orders'
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.titleWrapper}> 
+        <Text style={styles.largeTitle}>Management</Text>
+      </View>
+      <View style={styles.divider}/>
+      <TotalProducts/>
+      <Product/>
+      <Turnover/>
+      <Posts/>
+      <Customers/>
+      <Orders/>            
     </View>
-  );
+    
+  ) 
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+        flexDirection: 'column',        
+        backgroundColor: '#BEE9F6'
   },
+  titleWrapper:{
+    marginTop: 80,
+    paddingHorizontal: 16
+  },
+  largeTitle: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    color: "#5BA84F"
+  },
+  divider: {
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: '#014CF9',
+    marginHorizontal: 16,
+    marginTop: 16,
+  }
 });
